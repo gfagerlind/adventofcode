436a197c8b49f0457b4f5e7feaa7c05fd2adbb0c
Gustav Fägerlind <gustav.fagerlind@ictech.se>
2024-12-15 10:53:18 +0100

    Slight cleanup

--- 15/nicesolve.py
+++ 15/nicesolve.py
@@ -1,6 +1,4 @@
 import sys
-import re
-import math
 
 inp = """\
 ##########
@@ -47,27 +45,30 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 # #.....#
 # #######
 
+
 # <vv<<^^<<^^
 # """
 def char2move(s):
-    if s == '<':
+    if s == "<":
         return -1j
-    if s == '>':
+    if s == ">":
         return 1j
-    if s == '^':
+    if s == "^":
         return -1
-    if s == 'v':
+    if s == "v":
         return 1
+
 
 def move2char(s):
     if s == -1j:
-        return '<'
+        return "<"
     if s == 1j:
-        return '>'
+        return ">"
     if s == -1:
-        return '^'
+        return "^"
     if s == 1:
-        return 'v'
+        return "v"
+
 
 def pp(s, lx, ly):
     for x in range(lx):
@@ -78,58 +79,59 @@ def pp(s, lx, ly):
                 print(".", end="")
         print("")
 
-def push(p, d, cords,attempt=False):
-    np = cords.get(p+d,'None')
-    if np == '#':
+
+def push(p, d, cords, attempt=False):
+    np = cords.get(p + d, "None")
+    if np == "#":
         return False
-    if np in '[]' and d in (-1,1):
-        if np == '[':
-            ps = p+1j
+    if np in "[]" and d in (-1, 1):
+        if np == "[":
+            ps = p + 1j
         else:
-            ps = p-1j
-        if not push(p+d,d,cords, attempt=True):
+            ps = p - 1j
+        if not push(p + d, d, cords, attempt=True):
             return False
-        if not push(ps+d,d,cords, attempt=True):
+        if not push(ps + d, d, cords, attempt=True):
             return False
         if attempt:
             return True
         else:
-            push(p+d, d, cords)
-            push(ps+d,d,cords)
-    elif np in 'O[]' and not push(p+d, d, cords):
+            push(p + d, d, cords)
+            push(ps + d, d, cords)
+    elif np in "O[]" and not push(p + d, d, cords):
         # no attempt needed due since Os are only in variant 1,
         # and [] pushed up or down are caught above
         return False
     if p in cords and not attempt:
-        cords[p+d] = cords.pop(p)
+        cords[p + d] = cords.pop(p)
     return True
+
 
 with open(sys.argv[1]) as f:
     inp = f.read()
     inp_s, moves = inp.split("\n\n")
-    inp_s = inp_s.split('\n')
-    cords1 = {
-        x + y * 1j: inp_s[x][y] for x in range(len(inp_s)) for y in range(len(inp_s[x]))
-    }
-    cords1['@'] = [k for k,v in cords1.items() if v =="@"][0]
+    inp_s = inp_s.split("\n")
+    cords1 = {x + y * 1j: v for x, r in enumerate(inp_s) for y, v in enumerate(r)}
+    cords1["@"] = [k for k, v in cords1.items() if v == "@"][0]
     cords2 = {}
-    for x in range(len(inp_s)):
-        for y in range(len(inp_s[x])):
+    for x, r in enumerate(inp_s):
+        for y, v in enumerate(r):
             v = inp_s[x][y]
-            bp = x + y*2 * 1j
-            if v == '#':
+            bp = x + y * 2 * 1j
+            if v == "#":
                 cords2[bp] = v
                 cords2[bp + 1j] = v
-            if v == 'O':
-                cords2[bp] = '['
-                cords2[bp + 1j] = ']'
-            if v == '@':
-                cords2[bp] = '@'
-                cords2['@'] = bp
-    moves = [char2move(m) for m in moves.replace('\n','')]
-    for i,m in enumerate(moves):
+            if v == "O":
+                cords2[bp] = "["
+                cords2[bp + 1j] = "]"
+            if v == "@":
+                cords2[bp] = "@"
+                cords2["@"] = bp
+    moves = [char2move(m) for m in moves.replace("\n", "")]
+    for i, m in enumerate(moves):
         for cords in cords1, cords2:
-            if push(cords['@'], m, cords):
-                cords['@'] += m
-    print(sum(100 * p.real + p.imag for p,v in cords1.items() if p != '@' and v in 'O['))
-    print(sum(100 * p.real + p.imag for p,v in cords2.items() if p != '@' and v in 'O['))
+            if push(cords["@"], m, cords):
+                cords["@"] += m
+    for c in cords1, cords:
+        del c["@"]
+        print(sum(100 * p.real + p.imag for p, v in c.items() if v in "O["))
