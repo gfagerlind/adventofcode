b13a683db9518bfaaa1a8b1ed41a390e2d61c78b
Gustav Fägerlind <gustav.fagerlind@ictech.se>
2024-12-16 19:58:35 +0100

    Nicer

--- 16/nicesolve.py
+++ 16/nicesolve.py
@@ -37,41 +37,57 @@ inp = """\
 #S..#.....#...#
 ###############
 """
+
+
 def pp(s, lx, ly):
     for x in range(lx):
         for y in range(ly):
             if (x + 1j * y) in s:
-                print('o', end="")
+                print("o", end="")
             else:
                 print(".", end="")
         print("")
 
+
 with open(sys.argv[1]) as f:
     inp = f.read()
-    inp = inp.split('\n')
-    valid = {x + y * 1j for x, r in enumerate(inp) for y, v in enumerate(r) if v in '.E'}
-    startstop = {v: x + y * 1j for x, r in enumerate(inp) for y, v in enumerate(r) if v in 'ES'}
-    candidates = [(startstop['S'],set(),1j,0)]
-    stop = startstop['E']
+    inp = inp.split("\n")
+    valid = {
+        x + y * 1j for x, r in enumerate(inp) for y, v in enumerate(r) if v in ".E"
+    }
+    startstop = {
+        v: x + y * 1j for x, r in enumerate(inp) for y, v in enumerate(r) if v in "ES"
+    }
+    candidates = [(startstop["S"], set(), 1j, 0)]
+    stop = startstop["E"]
+    # [(point, direction): cost]
     visited = {}
-    res = {}
+    seats = set()
+    mincost = None
     while len(candidates):
-        c = candidates.pop(0)
-        p = c[0]
-        if visited.get(p, 9999999999999999) + 1000 < c[3]:
+        p, route, pd, c = candidates.pop(0)
+        if mincost and c > mincost:
             continue
-        visited[p] = c[3]
+        if (p, pd) in visited and visited[(p, pd)] < c:
+            # if we have already visited this point from this direction skip, and the cost is worse
+            # We need to consider equal paths for part2, otherwise the cost could be skipped
+            continue
+        visited[(p, pd)] = c
         if p == stop:
-            if c[3] in res:
-                res[c[3]] = res[c[3]] | {*c[1]}
-            else:
-                res[c[3]] = {*c[1]}
-        for d in (1,-1,1j,-1j):
-            cost = c[3] + 1 + (1000 if c[2] != d else 0)
+            if mincost == None or c < mincost:
+                mincost = c
+                seats = route
+            if c == mincost:
+                seats |= route
+        for d in (1, -1, 1j, -1j):
             if p + d not in valid:
                 continue
-            if p + d in c[1]:
+            if p + d in route:
+                # if this route has already traversed this point
                 continue
-            bisect.insort(candidates, (p+d, [*c[1], p + d], d, cost), key=lambda x: x[3])
-    print(min(res))
-    print(len(res[min(res)]) + 1)
+            cost = c + 1 + (1000 if pd != d else 0)
+            bisect.insort(
+                candidates, (p + d, route | {p + d}, d, cost), key=lambda x: x[3]
+            )
+    print(mincost)
+    print(len(seats) + 1)
