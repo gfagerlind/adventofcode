cb28dee76b55e90e61f33bf788d8c55451698bf1
Gustav Fägerlind <gustav.fagerlind@ictech.se>
2025-12-04 18:36:46 +0100

    Add 4

--- 4/nicesolve.py
+++ 4/nicesolve.py
@@ -1,68 +1,32 @@
 import sys
-from itertools import product
-
-test = [
-    "EFGXHIJ",
-    "1234567",
-    "890abcd",
-    "efgxhij",
-    "klmnopq",
-    "rstuvxy",
-    "RSTUVXY",
-]
-inp = [
-    "MMMSXXMASM",
-    "MSAMXMSMSA",
-    "AMXSXMAAMM",
-    "MSAMASMSMX",
-    "XMASAMXAMM",
-    "XXAMMXXAMA",
-    "SMSMSASXSS",
-    "SAXAMASAAA",
-    "MAMMMXMMMM",
-    "MXMXAXMASX",
-]
-
-p = (-1, 0, 1)
-PERMS = list(product(p, p))
-
-
-def get_bounded(m, x: int, y: int) -> str:
-    if 0 <= x < len(m) and 0 <= y < len(m[x]):
-        return m[x][y]
-    return ""
-
-
-def get(sx, sy, m, x, y, start, stop) -> str:
-    return "".join([get_bounded(m, x + sx * i, y + sy * i) for i in range(start, stop)])
-
-
-def count(m, x: int, y: int) -> int:
-    if m[x][y] != "X":
-        return 0
-    return sum([get(sx, sy, m, x, y, 0, 4) == "XMAS" for sx, sy in PERMS])
-
-
-def xcount(m, x: int, y: int) -> int:
-    if m[x][y] != "A":
-        return 0
-    if get(1, -1, m, x, y, -1, 2) in ("MAS", "SAM"):
-        if get(1, 1, m, x, y, -1, 2) in ("MAS", "SAM"):
-            return 1
-    return 0
-
 
 with open(sys.argv[1]) as f:
-    m = f.readlines()
-    print(
-        [
-            sum(x)
-            for x in zip(
-                *[
-                    (count(m, x, y), xcount(m, x, y))
-                    for x in range(len(m))
-                    for y in range(len(m[x]))
+    rolls = {
+        x + y * 1j
+        for x, l in enumerate(f.read().split("\n"))
+        for y, alpha in enumerate(l)
+        if alpha == "@"
+    }
+    start = len(rolls)
+    first = True
+    while True:
+        before = len(rolls)
+        rolls = {
+            r
+            for r in rolls
+            if not 4
+            >= len(
+                [
+                    r + dx + dy * 1j
+                    for dx in range(-1, 2)
+                    for dy in range(-1, 2)
+                    if r + dx + dy * 1j in rolls
                 ]
             )
-        ]
-    )
+        }
+        if first:
+            print(start - len(rolls))
+            first = False
+        if len(rolls) == before:
+            break
+    print(start - len(rolls))
